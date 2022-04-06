b9e0c82db370dbabc4797faf26ce110eddaed33d
yyliziqiu <yyliziqiu@163.com>
2022-04-06 13:35:26 +0800

    fix

--- response/response.go
+++ response/response.go
@@ -31,64 +31,79 @@ func Ok(c *gin.Context) {
 /**
 响应成功
 */
-func Success(c *gin.Context, result interface{}) {
+func Result(c *gin.Context, result interface{}) {
 	c.JSON(http.StatusOK, result)
 }
 
 /**
 请求终止
 */
-func Abort(c *gin.Context, err *errs.Error) {
-	c.AbortWithStatusJSON(err.StatusCode, newErrorResponse(err.Code, err.Message))
+func Abort(c *gin.Context, err error) {
+	e := prepareError(err)
+	c.AbortWithStatusJSON(e.StatusCode, newErrorResponse(e.Code, e.Message))
 }
 
 /**
 请求终止
 */
-func AbortWithError(c *gin.Context, err *errs.Error, err2 error) {
-	c.AbortWithStatusJSON(err.StatusCode, newErrorResponse(err.Code, err2.Error()))
+func AbortWithError(c *gin.Context, err error, err2 error) {
+	e := prepareError(err)
+	c.AbortWithStatusJSON(e.StatusCode, newErrorResponse(e.Code, err2.Error()))
 }
 
 /**
 请求终止
 */
-func AbortWithMessage(c *gin.Context, err *errs.Error, message string) {
-	c.AbortWithStatusJSON(err.StatusCode, newErrorResponse(err.Code, message))
+func AbortWithMessage(c *gin.Context, err error, message string) {
+	e := prepareError(err)
+	c.AbortWithStatusJSON(e.StatusCode, newErrorResponse(e.Code, message))
 }
 
 /**
 请求终止
 */
-func AbortWithMessageF(c *gin.Context, err *errs.Error, message string, a ...interface{}) {
-	c.AbortWithStatusJSON(err.StatusCode, newErrorResponse(err.Code, fmt.Sprintf(message, a...)))
+func AbortWithMessageF(c *gin.Context, err error, message string, a ...interface{}) {
+	e := prepareError(err)
+	c.AbortWithStatusJSON(e.StatusCode, newErrorResponse(e.Code, fmt.Sprintf(message, a...)))
 }
 
 /**
 请求失败
 */
-func Fail(c *gin.Context, err *errs.Error) {
-	c.JSON(err.StatusCode, newErrorResponse(err.Code, err.Message))
+func Fail(c *gin.Context, err error) {
+	e := prepareError(err)
+	c.JSON(e.StatusCode, newErrorResponse(e.Code, e.Message))
 }
 
 /**
 请求失败
 */
-func FailWithError(c *gin.Context, err *errs.Error, err2 error) {
-	c.JSON(err.StatusCode, newErrorResponse(err.Code, err2.Error()))
+func FailWithError(c *gin.Context, err error, err2 error) {
+	e := prepareError(err)
+	c.JSON(e.StatusCode, newErrorResponse(e.Code, err2.Error()))
 }
 
 /**
 请求失败
 */
-func FailWithMessage(c *gin.Context, err *errs.Error, message string) {
-	c.JSON(err.StatusCode, newErrorResponse(err.Code, message))
+func FailWithMessage(c *gin.Context, err error, message string) {
+	e := prepareError(err)
+	c.JSON(e.StatusCode, newErrorResponse(e.Code, message))
 }
 
 /**
 请求失败
 */
-func FailWithMessageF(c *gin.Context, err *errs.Error, message string, a ...interface{}) {
-	c.JSON(err.StatusCode, newErrorResponse(err.Code, fmt.Sprintf(message, a...)))
+func FailWithMessageF(c *gin.Context, err error, message string, a ...interface{}) {
+	e := prepareError(err)
+	c.JSON(e.StatusCode, newErrorResponse(e.Code, fmt.Sprintf(message, a...)))
+}
+
+func prepareError(err error) *errs.Error {
+	if e, ok := err.(*errs.Error); ok {
+		return e
+	}
+	return errs.BadRequest
 }
 
 /**
